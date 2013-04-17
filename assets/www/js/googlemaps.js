3744b4100f75727efee7730c59124d9405992c0c
aCasini <kasoale@gmail.com>
2013-04-17 18:25:27 +0200

    Introduzione di jQuery Mobile

--- assets/www/js/googlemaps.js
+++ assets/www/js/googlemaps.js
@@ -1,36 +1,36 @@
 function GoogleMap(){
-	/**
-	 * Option Var per la mappa
-	 */
-	this.mapOptions;
-	/**
-	 * Mappa 
-	 */
-	this.map;
-	
-	/**
-	 * Creazione della Mappa
-	 */
-	this.createGoogleMap = function(){
-		mapOptions = {
-			zoom: 8,
-			//Center the map
-			center: new google.maps.LatLng(44.355278,8.953857),
-			mapTypeId: google.maps.MapTypeId.ROADMAP 	//Tipologia di mappa
-		}
-		map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);	
-	}
-	
-	this.initializeMap = function(){
-		//Inizializza la mappa 
-		map.initialize();
-	}
-	
-	/**
-	 * Aggiunge il marker della geolocalizzazione alla mappa
-	 * @Param: map, la mappa sulla quale aggiungere il marker
-	 */
-	this.addMarkersToMap = function(map){
-		
-	}
+    
+    this.initialize = function(position){
+        var map = showMap();
+        addMarkersToMap(map, position);
+    }    
+    
+    var addMarkersToMap = function(map, position){
+        var mapBounds = new google.maps.LatLngBounds();
+    
+        var latitudeAndLongitudeOne = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+        var markerOne = new google.maps.Marker({
+					position: latitudeAndLongitudeOne,
+					map: map
+				});
+
+		//PER EFFETTUARE LO ZOOM AL EXTENT DEL MARKER
+        //mapBounds.extend(latitudeAndLongitudeOne);
+        //map.fitBounds(mapBounds);
+    }
+    
+    
+    
+    var showMap = function(){
+        var mapOptions = {
+			     zoom: 8,
+			     center: new google.maps.LatLng(44.355278,8.953857),
+			     mapTypeId: google.maps.MapTypeId.ROADMAP
+			 }
+
+        var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        
+        return map;
+    }
 }
